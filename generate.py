f48ec17077a3a285658ec87335bf811442e4e915
smhsimon <91816189+smhsimon@users.noreply.github.com>
2023-01-16 20:08:29 -0600

    testing joints 3

--- generate.py
+++ generate.py
@@ -1,23 +1,40 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-length = 1
-width = 1
-height = 1
+def CreateWorld():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[3, 2, 0.5] , size=[1, 1, 1])
+    pyrosim.End()
 
-x = 0
-y = 0
-z = .5
+def CreateRobot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1.5] , size=[1, 1, 1])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0.5,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[.5, 0, -.5] , size=[1, 1, 1])
+    
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [-.5,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-.5, 0, -.5] , size=[1, 1, 1])
+    pyrosim.End()
 
-a = 0
-b = 0
-c = .5
+CreateWorld()
+CreateRobot()
+# pyrosim.Start_SDF("world.sdf")
+# length = 1
+# width = 1
+# height = 1
 
-for i in range (5):
-    for j in range (5):
-        for k in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[i, j, c + k] , size=[length * ((.9) ** k), width * ((.9) ** k), height * ((.9) ** k)])
+# x = 0
+# y = 0
+# z = .5
 
-#pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
-#pyrosim.Send_Cube(name="Box2", pos=[a, b, c] , size=[length, width, height])
-pyrosim.End()
+# a = 0
+# b = 0
+# c = .5
+
+# # for i in range (5):
+# #     for j in range (5):
+# #         for k in range(10):
+# #             pyrosim.Send_Cube(name="Box", pos=[i, j, c + k] , size=[length * ((.9) ** k), width * ((.9) ** k), height * ((.9) ** k)])
+
+# pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+# #pyrosim.Send_Cube(name="Box2", pos=[a, b, c] , size=[length, width, height])
+# pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -8,9 +8,10 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 
-p.loadSDF("box.sdf")
-for i in range(1000):
+p.loadSDF("world.sdf")
+for i in range(10000):
     p.stepSimulation()
     time.sleep(1 / 60)
     print(i)
